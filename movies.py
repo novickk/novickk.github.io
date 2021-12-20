d62988b3739c693b6b2e098ae0e71c7c26edfdea
novickk <96434512+novickk@users.noreply.github.com>
2021-12-20 12:15:20 -0500

    Delete movies.py

--- movies.py
+++
@@ -1,7 +0,0 @@
-Predicting the Reviews of a Movie with IMDB
-Patrick Wang
-
-
-Introduction
-IMDB is a website where users and critics alike can review movies and input their opinions where everyone can see. While it supports more features than just reviews, IMDB is predominantly used in conjunction with other movie review websites like Rotten Tomatos to predict if a movie will be good or bad before people even watch them. Users can leave a positive or negative review and attach a short phrase that summarizes their viewpoints. Many times, a positive or negative aggregate score on these movie review websites can heavily influence people into watching the movie or not. For those on the fence about watching a movie, a positive review by another user could make or break their decision.
-In this tutorial, we will be attempting to use a dataset from IMDB with positive and negative reviews and try to extract insight as to if a movie review will be good or bad.
